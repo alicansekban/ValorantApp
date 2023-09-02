2c5ca5a442cdefa3694f95bbb702af0b7f7d0fde
alicansekban <alicansekban@hotmail.com>
2023-09-02 17:57:21 +0300

    added search to BundlesScreen

--- app/src/main/java/com/example/composestarter/presentation/bundles/BundlesScreen.kt
+++ app/src/main/java/com/example/composestarter/presentation/bundles/BundlesScreen.kt
@@ -1,5 +1,3 @@
-
-
 package com.example.composestarter.presentation.bundles
 
 import android.widget.Toast
@@ -7,22 +5,28 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.GridItemSpan
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -45,8 +49,10 @@ import com.example.composestarter.utils.ScreenRoutes
 @Composable
 fun BundlesScreen(
     viewModel: BundlesViewModel = hiltViewModel(),
-    onBackClicked : (String) -> Unit = {}
+    onBackClicked: (String) -> Unit = {}
 ) {
+
+    var searchQuery by remember { mutableStateOf("") }
 
     val bundles by viewModel.bundles.collectAsStateWithLifecycle()
     val context = LocalContext.current
@@ -80,8 +86,12 @@ fun BundlesScreen(
 @Composable
 fun StatelessBundlesScreen(
     bundles: List<BundlesUIModel>,
-    onBackClicked : (String) -> Unit = {}
+    onBackClicked: (String) -> Unit = {},
 ) {
+
+    var searchQuery by remember { mutableStateOf("") }
+
+    val filteredBundles = filterBundles(bundles, searchQuery)
 
     Scaffold { paddingValues ->
         Column {
@@ -99,9 +109,29 @@ fun StatelessBundlesScreen(
                         onBackClicked(ScreenRoutes.MoreRoute)
                     }
                 }
+                item(
+                    span = {
+                        GridItemSpan(maxCurrentLineSpan)
+                    }
+                ) {
+                    OutlinedTextField(
+                        value = searchQuery, onValueChange = {
+                            searchQuery = it
+                        },
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(start = 16.dp, end = 16.dp, top = 16.dp),
+                        shape = CircleShape,
+                        placeholder = {
+                            Text(text = "Search...")
+                        },
+                        maxLines = 1,
+                        singleLine = true
+                    )
+                }
 
                 items(
-                    items = bundles,
+                    items = filteredBundles,
                     key = { bundle -> bundle.uuid!! }
                 ) { value ->
                     BundleItem(value)
@@ -113,6 +143,18 @@ fun StatelessBundlesScreen(
     }
 
 }
+fun filterBundles(bundles: List<BundlesUIModel>, query: String): List<BundlesUIModel> {
+    return if (query.isBlank()) {
+        // Eğer sorgu boşsa, tüm öğeleri göster
+        bundles
+    } else {
+        // Aksi halde sorguya göre öğeleri filtrele
+        bundles.filter { bundle ->
+            bundle.displayName?.contains(query, ignoreCase = true) == true
+        }
+    }
+}
+
 
 @OptIn(ExperimentalGlideComposeApi::class)
 @Composable
@@ -130,13 +172,18 @@ fun BundleItem(
     ) {
         Column(horizontalAlignment = Alignment.CenterHorizontally) {
 
-            Text(text = bundle.displayName ?: "", textAlign = TextAlign.Center, style = MaterialTheme.typography.bodyMedium, modifier = Modifier.padding(8.dp))
+            Text(
+                text = bundle.displayName ?: "",
+                textAlign = TextAlign.Center,
+                style = MaterialTheme.typography.bodyMedium,
+                modifier = Modifier.padding(8.dp)
+            )
             Spacer(modifier = Modifier.height(4.dp))
             GlideImage(
                 model = bundle.displayIcon.toString(),
                 contentDescription = "Bundle Image",
                 contentScale = ContentScale.Crop
-            ){
+            ) {
                 it.error(R.drawable.ic_placeholder)
                     .placeholder(R.drawable.ic_placeholder)
                     .load(bundle.displayIcon)
